2e91bf3d025f5f1586e914a1bbd414e21fdca424
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-08-08 16:17:59 +0900

    fin

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,6 +35,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
